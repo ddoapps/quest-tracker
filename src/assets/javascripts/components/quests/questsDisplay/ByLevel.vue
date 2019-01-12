264eb7b38149f453f4f1e86be55caeeadccd4d95
taplar <taplarhw@gmail.com>
2019-01-12 13:38:35 -0600

    Increase the height of the quest name div and make the padding consistent

--- src/assets/javascripts/components/quests/questsDisplay/ByLevel.vue
+++ src/assets/javascripts/components/quests/questsDisplay/ByLevel.vue
@@ -1,19 +1,37 @@
 <template>
-    <div class="font-color-white-0 font-size-0 layout-col-xs-12 margin-top-8px">
-        <div class="font-size-100 font-weight-bold layout-col-xs-1 text-center" v-text="questType[ 0 ].toUpperCase()"></div>
-        <div :class="questType +'-background-color-dark-red-8 '+ questType +'-background-color-dark-purple-8'" class="border-top-left-radius-4px border-top-right-radius-4px font-size-150 layout-col-xs-11 text-indent-4px" v-text="name()"></div>
-        <div :class="questType +'-background-color-dark-red-8 '+ questType +'-background-color-dark-purple-8'" class="border-bottom-left-radius-4px border-top-left-radius-4px font-size-100 layout-col-xs-1 line-height-2-9 text-center vertical-align-top" v-text="level()"></div>
+    <div class="font-color-white-0 font-size-0 layout-col-xs-12 margin-bottom-4px-last-child margin-top-4px">
+        <div class="font-size-100 font-weight-bold layout-col-xs-1 text-center vertical-align-bottom"
+             v-text="questType[ 0 ].toUpperCase()"></div>
+        <div :class="questType +'-background-color-dark-red-8 '+ questType +'-background-color-dark-purple-8'"
+             class="border-top-left-radius-4px border-top-right-radius-4px font-size-130 height-44px layout-col-xs-11">
+            <span class="display-block margin-left-4px" v-text="name()"></span>
+        </div>
+        <div :class="questType +'-background-color-dark-red-8 '+ questType +'-background-color-dark-purple-8'"
+             class="border-bottom-left-radius-4px border-top-left-radius-4px font-size-100 layout-col-xs-1 line-height-2-9 text-center vertical-align-top"
+             v-text="level()"></div>
 
         <div class="layout-col-xs-11">
-            <div :class="questType +'-background-color-dark-red-3 '+ questType +'-background-color-dark-purple-3'" class="font-size-100 font-weight-bold layout-col-xs-3 text-indent-4px">Casual</div>
-            <div :class="questType +'-background-color-dark-red-0 '+ questType +'-background-color-dark-purple-0'" class="font-size-100 font-weight-bold layout-col-xs-3 text-indent-4px">Normal</div>
-            <div :class="questType +'-background-color-dark-red-3 '+ questType +'-background-color-dark-purple-3'" class="font-size-100 font-weight-bold layout-col-xs-3 text-indent-4px">Hard</div>
-            <div :class="questType +'-background-color-dark-red-0 '+ questType +'-background-color-dark-purple-0'" class="font-size-100 font-weight-bold layout-col-xs-3 text-indent-4px">Elite</div>
+            <div :class="questType +'-background-color-dark-red-3 '+ questType +'-background-color-dark-purple-3'"
+                 class="font-size-100 font-weight-bold layout-col-xs-3 text-indent-4px">Casual</div>
+            <div :class="questType +'-background-color-dark-red-0 '+ questType +'-background-color-dark-purple-0'"
+                 class="font-size-100 font-weight-bold layout-col-xs-3 text-indent-4px">Normal</div>
+            <div :class="questType +'-background-color-dark-red-3 '+ questType +'-background-color-dark-purple-3'"
+                 class="font-size-100 font-weight-bold layout-col-xs-3 text-indent-4px">Hard</div>
+            <div :class="questType +'-background-color-dark-red-0 '+ questType +'-background-color-dark-purple-0'"
+                 class="font-size-100 font-weight-bold layout-col-xs-3 text-indent-4px">Elite</div>
 
-            <div :class="questType +'-background-color-dark-red-3 '+ questType +'-background-color-dark-purple-3'" class="font-size-100 layout-col-xs-3 text-indent-4px" v-html="casual().xp"></div>
-            <div :class="questType +'-background-color-dark-red-0 '+ questType +'-background-color-dark-purple-0'" class="font-size-100 layout-col-xs-3 text-indent-4px" v-html="normal().xp"></div>
-            <div :class="questType +'-background-color-dark-red-3 '+ questType +'-background-color-dark-purple-3'" class="font-size-100 layout-col-xs-3 text-indent-4px" v-html="hard().xp"></div>
-            <div :class="questType +'-background-color-dark-red-0 '+ questType +'-background-color-dark-purple-0'" class="border-bottom-right-radius-4px font-size-100 layout-col-xs-3 text-indent-4px" v-html="elite().xp"></div>
+            <div :class="questType +'-background-color-dark-red-3 '+ questType +'-background-color-dark-purple-3'"
+                 class="font-size-100 layout-col-xs-3 text-indent-4px"
+                 v-html="casual().xp"></div>
+            <div :class="questType +'-background-color-dark-red-0 '+ questType +'-background-color-dark-purple-0'"
+                 class="font-size-100 layout-col-xs-3 text-indent-4px"
+                 v-html="normal().xp"></div>
+            <div :class="questType +'-background-color-dark-red-3 '+ questType +'-background-color-dark-purple-3'"
+                 class="font-size-100 layout-col-xs-3 text-indent-4px"
+                 v-html="hard().xp"></div>
+            <div :class="questType +'-background-color-dark-red-0 '+ questType +'-background-color-dark-purple-0'"
+                 class="border-bottom-right-radius-4px font-size-100 layout-col-xs-3 text-indent-4px"
+                 v-html="elite().xp"></div>
         </div>
     </div>
 </template>
