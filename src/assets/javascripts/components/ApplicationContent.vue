7e54c5583dc6d57d85fc8d01ba1c899a3ee77fca
taplar <taplarhw@gmail.com>
2019-02-23 15:20:02 -0600

    Do not display the main menu if the search input is focused

--- src/assets/javascripts/components/ApplicationContent.vue
+++ src/assets/javascripts/components/ApplicationContent.vue
@@ -1,5 +1,6 @@
 <template>
-	<main class="bottom-above-menu center display-inline-block margin-left-1vw max-width-98vw min-width-98vw overflow-y-auto position-absolute position-top z-index-100"
+	<main	class="center display-inline-block margin-left-1vw max-width-98vw min-width-98vw overflow-y-auto position-absolute position-top z-index-100"
+			:class="ifTheMainMenuIsShown() ? 'bottom-above-menu' : 'position-bottom'"
 		@scroll="updateTheMainScrollTop($event)">
 		<disclaimer v-if="theDisclaimerShouldBeShown"></disclaimer>
 		<quests v-if="!theDisclaimerShouldBeShown"></quests>
@@ -21,7 +22,10 @@
 			}
 		}
 		, methods: {
-			updateTheMainScrollTop: function ( event ) {
+			ifTheMainMenuIsShown: function () {
+				return !this.$store.getters.theQuestListingsOmniSearchShouldBeShown
+			}
+			, updateTheMainScrollTop: function ( event ) {
 				this.$store.commit( 'updateTheMainScrollTop', event.target.scrollTop );
 			}
 		}
