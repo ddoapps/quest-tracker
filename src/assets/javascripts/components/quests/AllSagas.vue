10cd4c6218d52bd0cf5eb103dd39cda9ff78b084
taplar <taplarhw@gmail.com>
2019-02-27 21:05:36 -0600

    Fix issue with passing in filtered quests and filtered sagas not showing all quests

--- src/assets/javascripts/components/quests/AllSagas.vue
+++ src/assets/javascripts/components/quests/AllSagas.vue
@@ -1,7 +1,7 @@
 <template>
 	<section>
-		<saga sagaType="heroic" v-for="(saga, index) in filteredHeroicSagas" :key="saga.id +'heroic'" :saga="saga" :sagaIndex="index" :quests="filteredHeroicQuests"></saga>
-		<saga sagaType="epic" v-for="(saga, index) in filteredEpicSagas" :key="saga.id +'epic'" :saga="saga" :sagaIndex="index" :quests="filteredEpicQuests"></saga>
+		<saga sagaType="heroic" v-for="(saga, index) in filteredHeroicSagas" :key="saga.id +'heroic'" :saga="saga" :sagaIndex="index" :quests="heroicQuests"></saga>
+		<saga sagaType="epic" v-for="(saga, index) in filteredEpicSagas" :key="saga.id +'epic'" :saga="saga" :sagaIndex="index" :quests="epicQuests"></saga>
 	</section>
 </template>
 
